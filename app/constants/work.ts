4270c1bd118dd5a07402130061af0057c5cb5201
nastyafowkes <nastya.karasevych1@gmail.com>
2026-04-23 13:47:33 +0300

    Update work.ts

--- app/constants/work.ts
+++ app/constants/work.ts
@@ -5,22 +5,22 @@ export const WORK_TIMELINE: WorkTimelinePoint[] = [
   {
     point: new THREE.Vector3(0, 0, 0),
      year: '2022',
-    title: 'Web Design Projects',
+    title: 'Web Design',
     subtitle: 'Personal Portfolio',
     position: 'right',
   },
   {
     point: new THREE.Vector3(-4, -4, -3),
     year: '2023',
-    title: 'Backroads Charitable Foundation',
+    title: 'Backroads Foundation',
     subtitle: 'Data Analyst Intern and English-Ukrainian Translator',
     position: 'left',
   },
   {
     point: new THREE.Vector3(-3, -1, -6),
     year: '2024',
-    title: 'Web Designer Intern and English Teacher',
-    subtitle: 'Self-employed',
+    title: 'Web Designer',
+    subtitle: 'Self-employed as an Intern and English Teacher',
     position: 'left',
   },
   {
@@ -33,7 +33,7 @@ export const WORK_TIMELINE: WorkTimelinePoint[] = [
   {
     point: new THREE.Vector3(1, 1, -12),
      year: '2026',
-    title: 'Fiverr and BYU Companion',
+    title: 'Fiverr&Companion',
     subtitle: 'Web Design & Development & Data Analyst',
     position: 'right',
   }
